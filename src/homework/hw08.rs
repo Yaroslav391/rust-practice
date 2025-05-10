8f6ef3034fbfa14213fa2afe99764f124be842ff
Yaroslav <yaroslav.bytsak@e-u.edu.ua>
2025-05-10 20:14:59 +0300

    Update hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,36 @@
+pub fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
+
+    let sqrt_n = (n as f64).sqrt() as u64;
+    for i in (3..=sqrt_n).step_by(2) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_prime() {
+        assert!(!is_prime(0));
+        assert!(!is_prime(1));
+        assert!(is_prime(2));
+        assert!(is_prime(3));
+        assert!(!is_prime(4));
+        assert!(is_prime(5));
+        assert!(!is_prime(9));
+        assert!(is_prime(13));
+        assert!(!is_prime(100));
+        assert!(is_prime(101));
+    }
+}
